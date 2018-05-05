25417d140919c743486c2e87f6dfc34bb84c708b
GuilhermeMartinez <guigui.martinez20@hotmail.com>
2018-05-04 22:29:03 -0300

    Alteração

---
+++ RecGuimMobi/RecGuimMobi/Views/DetalheView.xaml.cs
@@ -0,0 +1,40 @@
+﻿using RecGuimMobi.ViewsModels;
+using RecGuimMobi.Classes;
+using RecGuimMobi.ViewsModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RecGuimMobi.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DetalheView : ContentPage
+    {
+
+        public Noticias Noticias { get; set; }
+        public DetalheViewModel ViewModel { get; set; }
+        public DetalheView(Noticias noticias)
+        {
+            InitializeComponent();
+            Noticias = noticias;
+            this.ViewModel = new DetalheViewModel(noticias);
+            this.BindingContext = ViewModel;
+        }
+
+        
+
+        protected override void OnAppearing()
+        {
+            //base.OnAppearing();
+            //MessagingCenter.Subscribe<Restaurante>(this, "Proximo",
+            //(msg) =>
+            //{
+            //    Navigation.PushAsync(new AgendamentoView(msg));
+            //});
+            base.OnAppearing();
+            photoImage.Source = Noticias.PhotoFullPath;
+            photoImage.HeightRequest = 200f;
+            photoImage.WidthRequest = 200f;
+        }
+
+    }
+}
